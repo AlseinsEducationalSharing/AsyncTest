0896e48fe25e3e3b2d9d01c72601b1cb90b28a13
Alsein <xyh951115@live.com>
2018-05-17 18:07:25 +1000

    initial commit

---
+++ AsyncTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = A();
+            Console.WriteLine("A is running...");
+            Console.WriteLine(task.Result);
+        }
+
+        private static async Task<int> A() => (await B()) + 1;
+
+        private static async Task<int> B()
+        {
+            await Task.Delay(3000);
+            return 5;
+        }
+    }
+}
