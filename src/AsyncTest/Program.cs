e464162f565fa28e0eb3ae58abc5d508b3fd7340
Alsein <xyh951115@live.com>
2018-05-17 22:00:12 +1000

    clear code

--- src/AsyncTest/Program.cs
+++ src/AsyncTest/Program.cs
@@ -7,17 +7,7 @@ namespace AsyncTest
     {
         static void Main(string[] args)
         {
-            var task = A();
-            Console.WriteLine("A is running...");
-            Console.WriteLine(task.Result);
-        }
-
-        private static async Task<int> A() => (await B()) + 1;
-
-        private static async Task<int> B()
-        {
-            await Task.Delay(3000);
-            return 10;
+            Console.WriteLine("Complete my mission!");
         }
     }
 }
